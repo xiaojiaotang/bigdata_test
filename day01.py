1fe371abcbb0932b0970d75ae37f044868c07081
xiaojiaotang <39868132+xiaojiaotang@users.noreply.github.com>
2018-06-03 09:37:42 +0800

    Add files via upload

---
+++ day01.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun  2 11:32:25 2018
+
+@author: lenovo
+"""
+
+week=["星期一","星期二","星期三","星期四","星期五","星期六","星期天"]
+for i in week:
+  if i =="星期六":
+    print("今天是"+i)
+else:
+    print(i)
